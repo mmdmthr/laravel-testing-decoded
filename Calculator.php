2799e3aa69a12c4dc30138c75e28c85cd9212ea1
mmdmthr <muhhari@gmail.com>
2023-04-26 17:34:44 +0700

    first refactor

--- Calculator.php
+++ Calculator.php
@@ -3,31 +3,39 @@
 class Calculator
 {
     protected $result = 0;
-    
-    public function add($num)
-    {
-        foreach (func_get_args() as $num) {
-            if (!is_numeric($num)) {
-                throw new InvalidArgumentException;
-            }
-    
-            $this->result += $num;
-        }
-    }
-
-    public function substract($num)
-    {
-        foreach (func_get_args() as $num) {
-            if (!is_numeric($num)) {
-                throw new InvalidArgumentException;
-            }
-    
-            $this->result -= $num;
-        }
-    }
 
     public function getResult()
     {
         return $this->result;
+    }
+    
+    public function add($num)
+    {
+        $this->calculateAll(func_get_args(), '+');
+    }
+
+    public function substract($num)
+    {
+        $this->calculateAll(func_get_args(), '-');
+    }
+
+    protected function calculateAll(array $nums, $symbol)
+    {
+        foreach ($nums as $num) {
+            $this->calculate($num, $symbol);
+        }
+    }
+
+    protected function calculate($num, $symbol)
+    {
+        if (!is_numeric($num)) throw new InvalidArgumentException;
+
+        switch ($symbol) {
+            case '+':
+                $this->result += $num;
+                break;
+            case '-':
+                $this->result -= $num;
+        }
     }
 }
